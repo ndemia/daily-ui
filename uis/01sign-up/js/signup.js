76d57d712a897e53dad95b0927a7d9566825f025
ndemia <info@demia.me>
2022-12-28 16:07:53 +0100

    Improve tabs look and feel, and redefine code in js

--- uis/01sign-up/js/signup.js
+++ uis/01sign-up/js/signup.js
@@ -1,28 +1,23 @@
-// UI variables
 const signUpTab = document.getElementById('signUp');
 const logInTab = document.getElementById('logIn');
 const mainSignUp = document.getElementById('user-access__sign-up-view');
 const mainLogIn = document.getElementById('user-access__log-in-view');
 const userAccess = document.getElementById('user-access');
 
-// Toggle elements on/off when showing the Log in screen
-logInTab.onclick = function showLogIn() {
-	userAccess.classList.add('user-access--new-height');
-	signUpTab.classList.replace('nav--active', 'nav--inactive');
-	logInTab.classList.replace('nav--inactive', 'nav--active');
-	logInTab.classList.remove('nav__logIn--inactive-shadow');
-	signUpTab.classList.add('nav__signUp--inactive-shadow');
+logInTab.addEventListener('click', () => {
+	signUpTab.classList.replace('tab--active', 'tab--inactive');
+	logInTab.classList.replace('tab--inactive', 'tab--active');
+	logInTab.classList.remove('tab--lftshadow');
+	signUpTab.classList.add('tab--rgtshadow');
 	mainSignUp.classList.replace('user-access--active', 'user-access--inactive');
 	mainLogIn.classList.replace('user-access--inactive', 'user-access--active');
-}
-
-// Toggle elements on/off when showing the Sign up screen
-signUpTab.onclick = function showSignUp() {
-	userAccess.classList.remove('user-access--new-height');
-	logInTab.classList.replace('nav--active', 'nav--inactive');
-	signUpTab.classList.replace('nav--inactive', 'nav--active');
-	signUpTab.classList.remove('nav__signUp--inactive-shadow');
-	logInTab.classList.add('nav__logIn--inactive-shadow');
+});
+	
+signUpTab.addEventListener('click', () => {
+	logInTab.classList.replace('tab--active', 'tab--inactive');
+	signUpTab.classList.replace('tab--inactive', 'tab--active');
+	signUpTab.classList.remove('tab--rgtshadow');
+	logInTab.classList.add('tab--lftshadow');
 	mainLogIn.classList.replace('user-access--active', 'user-access--inactive');
 	mainSignUp.classList.replace('user-access--inactive', 'user-access--active');
-}
+});
