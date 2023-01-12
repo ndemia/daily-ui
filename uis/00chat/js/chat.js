5db067febe1f91804216f9bf62f8543ce081df30
ndemia <info@demia.me>
2023-01-12 19:00:14 +0100

    Refactor the entire project in order to improve and update the code

--- uis/00chat/js/chat.js
+++ uis/00chat/js/chat.js
@@ -1,58 +1,65 @@
 // Add message to the conversation.
-const addMessageToConversation = function() {
+function addMessageToConversation () {
 
 	// Define variable to store message text.
-	let messageText = document.querySelector('.conversation__write').value;
+	let messageText = document.querySelector('.footer__input').value;
 
 	// If message is empty...
-	if ( messageText.trim().length == 0 ) {
+	if (messageText.trim().length == 0) {
 
-		// Do nothing.
 		return false;
 		
 	} else {
 
-		// Add the message text with the bubble to the conversation.
-		document.querySelector('.conversation__body').insertAdjacentHTML('beforeend', `<div class="conversation__bubble conversation__bubble--right"><p class="conversation__text">${messageText}</p></div>`);
+		// Add the message with the bubble to the conversation.
+		document.querySelector('.messages').insertAdjacentHTML('beforeend', `<div class="messages__bubble messages__bubble--right"><p class="messages__text">${messageText}</p></div>`);
 
 		// Clear the input.
-		document.querySelector('.conversation__write').value = '';
+		document.querySelector('.footer__input').value = '';
 
 		// Return focus to input.
-		document.querySelector('.conversation__write').focus();
+		document.querySelector('.footer__input').focus();
 	}
 
-};
+}
+
+
 
 // Show chat list.
-document.querySelector('.btn--list').addEventListener('click', function() {
+document.querySelector('.btn--list').addEventListener('click', function () {
 
 	document.querySelector('.container').classList.add('list--active');
 	
 });
 
+
+
+// Show conversation.
+document.querySelector('#chat1').addEventListener('click', function () {
+
+	document.querySelector('.container').classList.remove('list--active');
+
+});
+
+
+
 // Minimize chat.
-document.querySelectorAll('.conversation__info, .list__header').forEach(function(e) {
+document.querySelectorAll('.header__info, .list__header').forEach(function (e) {
 	
-	e.addEventListener('click', function() {
+	e.addEventListener('click', function () {
 
 		document.querySelector('.mask').classList.toggle('minimize');
 
 	});
 });
 
-// Show conversation.
-document.querySelector('#chat1').addEventListener('click', function() {
 
-	document.querySelector('.container').classList.remove('list--active');
-
-});
-
-// Send message with send button.
+// Events
+// Send message.
 document.querySelector('.btn--send').addEventListener('click', addMessageToConversation);
 
 // Send message with enter key.
-document.querySelector('.conversation__write').addEventListener('keydown', function(e) {
+document.querySelector('.footer__input').addEventListener('keydown', function (e) {
 
 	if (e.key == 'Enter') {
 
@@ -61,5 +68,6 @@ document.querySelector('.conversation__write').addEventListener('keydown', funct
 	} else {
 
 		return false;
+		
 	}
 });
