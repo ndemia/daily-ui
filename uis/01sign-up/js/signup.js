0ba875a81efc7efdf691507f0c3c04bb7cc1b40e
ndemia <info@demia.me>
2023-02-14 17:34:35 +0100

    Fix formatting width

--- uis/01sign-up/js/signup.js
+++ uis/01sign-up/js/signup.js
@@ -5,19 +5,19 @@ const mainLogIn = document.getElementById('user-access__log-in-view');
 const userAccess = document.getElementById('user-access');
 
 logInTab.addEventListener('click', () => {
-	signUpTab.classList.replace('tab--active', 'tab--inactive');
-	logInTab.classList.replace('tab--inactive', 'tab--active');
-	logInTab.classList.remove('tab--lftshadow');
-	signUpTab.classList.add('tab--rgtshadow');
-	mainSignUp.classList.replace('user-access--active', 'user-access--inactive');
-	mainLogIn.classList.replace('user-access--inactive', 'user-access--active');
+  signUpTab.classList.replace('tab--active', 'tab--inactive');
+  logInTab.classList.replace('tab--inactive', 'tab--active');
+  logInTab.classList.remove('tab--lftshadow');
+  signUpTab.classList.add('tab--rgtshadow');
+  mainSignUp.classList.replace('user-access--active', 'user-access--inactive');
+  mainLogIn.classList.replace('user-access--inactive', 'user-access--active');
 });
-	
+
 signUpTab.addEventListener('click', () => {
-	logInTab.classList.replace('tab--active', 'tab--inactive');
-	signUpTab.classList.replace('tab--inactive', 'tab--active');
-	signUpTab.classList.remove('tab--rgtshadow');
-	logInTab.classList.add('tab--lftshadow');
-	mainLogIn.classList.replace('user-access--active', 'user-access--inactive');
-	mainSignUp.classList.replace('user-access--inactive', 'user-access--active');
+  logInTab.classList.replace('tab--active', 'tab--inactive');
+  signUpTab.classList.replace('tab--inactive', 'tab--active');
+  signUpTab.classList.remove('tab--rgtshadow');
+  logInTab.classList.add('tab--lftshadow');
+  mainLogIn.classList.replace('user-access--active', 'user-access--inactive');
+  mainSignUp.classList.replace('user-access--inactive', 'user-access--active');
 });
